d6349b4c511e7e1fc52010c94cd25ff6baf3939f
Das-Horn <craigdoyle220301@gmail.com>
2021-10-07 12:43:10 +0100

    Title bar

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {HeadFrame} from './Frame';
+import Title from '../Title';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
 
 export function Header() {
     return (
         <HeadFrame>
-            <FormattedMessage {...messages.title}/>
+            <Title text={`Collage Site`} />
         </HeadFrame>
     );
 }
